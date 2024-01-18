1a8ebc72dab18bbe2314ec2e86b687f182e71d21
lenovo <lenovo@lenovo-PC-15>
2024-01-18 13:29:11 +0530

    First Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/ConpileInputDemo.java
@@ -0,0 +1,27 @@
+package com.edubridge.compileinput;
+//program to demonstrate on CompileTime Input of diff data types
+public class ConpileInputDemo {
+
+	public static void main(String[] args) {
+		byte num1=114;
+		short num2=3424;
+		int num3=32737273;
+		long num4=1213213232333333333L;
+		char c='X';
+		String str="Mufaddal";
+		float num5=67.4f;
+		double num6=67.4;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(c);
+		System.out.println(str);
+		System.out.println(num5);
+		System.out.println(num6);
+
+
+
+	}
+
+}
