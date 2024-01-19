a80eab72dc3ad12cef5ae2360d397c29a2350dd4
lenovo <lenovo@lenovo-PC-15>
2024-01-19 11:11:39 +0530

    Second Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/FloatingPointDataType.java
@@ -0,0 +1,19 @@
+package com.edubridge.compileinput;
+
+import java.util.Scanner;
+
+public class FloatingPointDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		float x = s.nextFloat();
+		double y = s.nextDouble();
+		System.out.println("value of float is "+ x);
+		System.out.println("value of float is "+ y);
+		s.close();
+
+	}
+
+}
---
+++ IntroToJava/src/com/edubridge/compileinput/StringAndBooleanDemo.java
@@ -0,0 +1,21 @@
+package com.edubridge.compileinput;
+
+import java.util.Scanner;
+
+public class StringAndBooleanDemo {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		boolean x = s.nextBoolean();
+		String y = s.next();
+		String a = s.nextLine();
+		char z = s.next().charAt(0);
+
+		System.out.println("value of boolean is "+ x);
+		System.out.println("The string is "+y);
+		System.out.println("The string line is "+a);
+		System.out.println("The char is "+z);
+
+		s.close();
+	}
+}
